2ff44eef3d18dd811fa0e1d2cbae0da3ca9e70ea
DreemurrDango <3011090456@qq.com>
2025-12-18 15:40:20 +0800

    添加自述文件和更新日志，移动洗牌算法功能到命名空间下，修改版本号以对同步工作流进行测试

--- Runtime/ListExtension.cs
+++ Runtime/ListExtension.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using System;
 
-public static class ListExtension
+namespace DreemurrStudio.CollectionExtension
 {
-    /// <summary>
-    /// 对 IList 列表进行原地洗牌乱序。
-    /// 使用 Fisher-Yates 算法。
-    /// </summary>
-    /// <typeparam name="T">列表元素的类型。</typeparam>
-    /// <param name="list">要进行洗牌的列表。</param>
-    /// <param name="seed">可选的随机数生成器种子。如果提供，将使用该种子初始化随机数生成器，以确保可重复的洗牌结果</param>
-    public static void Shuffle<T>(this IList<T> list,int? seed = null)
+    public static class ListExtension
     {
-        int n = list.Count;
-        var rng = seed.HasValue? new Random(seed.Value) : new Random();
-        while (n > 1)
+        /// <summary>
+        /// 对 IList 列表进行原地洗牌乱序。
+        /// 使用 Fisher-Yates 算法。
+        /// </summary>
+        /// <typeparam name="T">列表元素的类型。</typeparam>
+        /// <param name="list">要进行洗牌的列表。</param>
+        /// <param name="seed">可选的随机数生成器种子。如果提供，将使用该种子初始化随机数生成器，以确保可重复的洗牌结果</param>
+        public static void Shuffle<T>(this IList<T> list, int? seed = null)
         {
-            n--;
-            int k = rng.Next(n + 1);
-            // 交换 list[k] 和 list[n]
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
+            int n = list.Count;
+            var rng = seed.HasValue ? new Random(seed.Value) : new Random();
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                // 交换 list[k] 和 list[n]
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
