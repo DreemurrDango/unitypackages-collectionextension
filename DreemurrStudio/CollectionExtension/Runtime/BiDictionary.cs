59fdb4b6114808afd35ee86efa7ef508582780da
DreemurrDango <3011090456@qq.com>
2025-12-18 13:54:27 +0800

    已有内容更新

---
+++ DreemurrStudio/CollectionExtension/Runtime/BiDictionary.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 双向字典，允许通过键获取值，也允许通过值获取键，其中键和值均为唯一
+/// 注意：双向字典的两个类型不应该是相同的类型，以避免歧义
+/// </summary>
+public class BiDictionary<T1, T2> : IEnumerable<KeyValuePair<T1, T2>>
+{
+    /// <summary>
+    /// 正向字典
+    /// </summary>
+    private readonly Dictionary<T1, T2> _forward = new Dictionary<T1, T2>();
+    /// <summary>
+    /// 反向字典
+    /// </summary>
+    private readonly Dictionary<T2, T1> _reverse = new Dictionary<T2, T1>();
+
+    /// <summary>
+    /// 获取双向字典中的键值对数量
+    /// </summary>
+    public int Count => _forward.Count;
+
+    /// <summary>
+    /// 正向获取值
+    /// </summary>
+    /// <param name="key">正向键值</param>
+    /// <returns>键对应的值</returns>
+    public T2 this[T1 key]
+    {
+        get => _forward[key];
+        set
+        {
+            if (_forward.TryGetValue(key,out T2 oldValue))
+                _reverse.Remove(oldValue);
+            _forward[key] = value;
+            _reverse[value] = key;
+        }
+    }
+
+    /// <summary>
+    /// 反向获取键
+    /// </summary>
+    /// <param name="key2"></param>
+    /// <returns></returns>
+    public T1 this[T2 key2]
+    {
+        get => _reverse[key2];
+        set
+        {
+            if (_reverse.TryGetValue(key2, out T1 oldKey))
+                _forward.Remove(oldKey);
+            _reverse[key2] = value;
+            _forward[value] = key2;
+        }
+    }
+
+    /// <summary>
+    /// 添加新的键值对（键与值均应当唯一）
+    /// </summary>
+    /// <param name="key">要添加的键</param>
+    /// <param name="value">要添加的值</param>
+    /// <exception cref="System.ArgumentException"></exception>
+    public void Add(T1 key, T2 value)
+    {
+        if(_forward.ContainsKey(key) || _reverse.ContainsKey(value))
+            throw new System.ArgumentException($"双向键值 {key}:{value} 已存在于双向字典中");
+        _forward.Add(key, value);
+        _reverse.Add(value, key);
+    }
+
+    /// <summary>
+    /// 移除指定键对应的键值对
+    /// </summary>
+    /// <param name="key"></param>
+    public void Remove(T1 key)
+    {
+        if (_forward.TryGetValue(key, out T2 value))
+        {
+            _forward.Remove(key);
+            _reverse.Remove(value);
+        }
+    }
+
+    /// <summary>
+    /// 移除指定值对应的键值对
+    /// </summary>
+    /// <param name="value"></param>
+    public void Remove(T2 value)
+    {
+        if (_reverse.TryGetValue(value, out T1 key))
+        {
+            _reverse.Remove(value);
+            _forward.Remove(key);
+        }
+    }
+
+    public bool Contains(T1 key) => _forward.ContainsKey(key);
+    public bool Contains(T2 value) => _reverse.ContainsKey(value);
+    public bool TryGetValue(T1 key, out T2 value) => _forward.TryGetValue(key, out value);
+    public bool TryGetValue(T2 value, out T1 key) => _reverse.TryGetValue(value, out key);
+
+    /// <summary>
+    /// 移除双向字典中的所有键值对
+    /// </summary>
+    public void Clear()
+    {
+        _forward.Clear();
+        _reverse.Clear();
+    }
+
+    /// <summary>
+    /// 返回一个枚举器，按添加顺序遍历双向字典中的键值对
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator<KeyValuePair<T1, T2>> GetEnumerator() => _forward.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ DreemurrStudio/CollectionExtension/Runtime/Heap.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DreemurrStudio.CollectionExtension
+{
+    /// <summary>
+    /// 可堆排序的元素接口
+    /// </summary>
+    /// <typeparam name="T">堆排序的排序值类型</typeparam>
+    public interface IHeapItem<T> : IComparable<T>
+    {
+        /// <summary>
+        /// 获取或设置元素在堆中的索引
+        /// </summary>
+        public int HeapIndex
+        {
+            get;
+            set;
+        }
+    }
+
+    /// <summary>
+    /// 用于堆排序的二叉堆抽象数据结构
+    /// </summary>
+    /// <typeparam name="T">要排序的可比较元素类</typeparam>
+    public abstract class Heap<T> : IEnumerable<T> where T : IHeapItem<T>
+    {
+        /// <summary>
+        /// 堆排序元素数组
+        /// </summary>
+        protected T[] items;
+        /// <summary>
+        /// 当前堆中元素的数量
+        /// </summary>
+        protected int currentItemCount;
+
+        /// <summary>
+        /// 获取堆中元素的数量
+        /// </summary>
+        public int Count => currentItemCount;
+
+        public Heap(int maxHeapSize)
+        {
+            items = new T[maxHeapSize];
+        }
+
+        public T this[int index] => items[index];
+
+        /// <summary>
+        /// 返回是否包含指定的元素
+        /// </summary>
+        /// <param name="item">要查找的元素</param>
+        /// <returns></returns>
+        public bool Contains(T item)
+        {
+            if (item.HeapIndex < 0 || item.HeapIndex >= currentItemCount) return false;
+            return Equals(items[item.HeapIndex], item);
+        }
+
+        /// <summary>
+        /// 向堆中添加元素
+        /// </summary>
+        /// <param name="item">要添加的元素</param>
+        public void Push(T item)
+        {
+            item.HeapIndex = currentItemCount;
+            items[currentItemCount] = item;
+            ShiftUp(item);
+            currentItemCount++;
+        }
+
+        /// <summary>
+        /// 取出堆顶元素
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            T firstItem = items[0];
+            currentItemCount--;
+            items[0] = items[currentItemCount];
+            items[0].HeapIndex = 0;
+            ShiftDown(items[0]);
+            return firstItem;
+        }
+
+        /// <summary>
+        /// 更新堆中元素的位置
+        /// </summary>
+        /// <param name="item">要更新的元素</param>
+        public void UpdateItem(T item) => ShiftUp(item);
+
+        /// <summary>
+        /// 向上调整堆中元素的位置
+        /// </summary>
+        /// <param name="item"></param>
+        protected abstract void ShiftUp(T item);
+
+        /// <summary>
+        /// 向下调整堆中元素的位置
+        /// </summary>
+        /// <param name="item"></param>
+        protected abstract void ShiftDown(T item);
+
+        /// <summary>
+        /// 交换堆中两个元素的位置
+        /// </summary>
+        protected void Swap(T itemA, T itemB)
+        {
+            items[itemA.HeapIndex] = itemB;
+            items[itemB.HeapIndex] = itemA;
+            int itemAIndex = itemA.HeapIndex;
+            itemA.HeapIndex = itemB.HeapIndex;
+            itemB.HeapIndex = itemAIndex;
+        }
+
+        public IEnumerator<T> GetEnumerator() => items.GetEnumerator() as IEnumerator<T>;
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    /// <summary>
+    /// 最小二叉堆：堆顶元素为最小值
+    /// 常用于优先队列实现，可比较值越小优先级越高
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MinHeap<T> : Heap<T> where T : IHeapItem<T>
+    {
+        public MinHeap(int maxHeapSize) : base(maxHeapSize) { }
+
+        protected override void ShiftUp(T item)
+        {
+            int parentIndex = (item.HeapIndex - 1) / 2;
+            while (true)
+            {
+                T parentItem = items[parentIndex];
+                if (item.CompareTo(parentItem) < 0) Swap(item, parentItem);
+                else break;
+                parentIndex = (item.HeapIndex - 1) / 2;
+            }
+        }
+        protected override void ShiftDown(T item)
+        {
+            while (true)
+            {
+                int leftChildIndex = item.HeapIndex * 2 + 1;
+                int rightChildIndex = item.HeapIndex * 2 + 2;
+                int swapIndex = 0;
+                if (leftChildIndex < Count)
+                {
+                    swapIndex = leftChildIndex;
+                    if (rightChildIndex < Count)
+                    {
+                        if (items[rightChildIndex].CompareTo(items[leftChildIndex]) < 0)
+                            swapIndex = rightChildIndex;
+                    }
+                    if (items[swapIndex].CompareTo(item) < 0)
+                        Swap(item, items[swapIndex]);
+                    else return;
+                }
+                else return;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 最大二叉堆：堆顶元素为最大值，可比较值越大优先级越高
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MaxHeap<T> : Heap<T> where T : IHeapItem<T>
+    {
+        public MaxHeap(int maxHeapSize) : base(maxHeapSize) { }
+        protected override void ShiftUp(T item)
+        {
+            int parentIndex = (item.HeapIndex - 1) / 2;
+            while (true)
+            {
+                T parentItem = items[parentIndex];
+                if (item.CompareTo(parentItem) > 0) Swap(item, parentItem);
+                else break;
+                parentIndex = (item.HeapIndex - 1) / 2;
+            }
+        }
+        protected override void ShiftDown(T item)
+        {
+            while (true)
+            {
+                int leftChildIndex = item.HeapIndex * 2 + 1;
+                int rightChildIndex = item.HeapIndex * 2 + 2;
+                int swapIndex = 0;
+                if (leftChildIndex < Count)
+                {
+                    swapIndex = leftChildIndex;
+                    if (rightChildIndex < Count)
+                    {
+                        if (items[rightChildIndex].CompareTo(items[leftChildIndex]) > 0)
+                            swapIndex = rightChildIndex;
+                    }
+                    if (items[swapIndex].CompareTo(item) > 0)
+                        Swap(item, items[swapIndex]);
+                    else return;
+                }
+                else return;
+            }
+        }
+    }
+}
